f59407dd17394b3d890baf3c61f7f04474aff92b
Travis DePrato <travisdeprato@gmail.com>
2021-08-26 09:16:51 -0700

    Fix dependencies, use only setuptools

--- setup.py
+++ setup.py
@@ -3,18 +3,19 @@ import shutil
 import sys
 from glob import glob
 
+# Ironically, we can't use Poetry as the build system since it doesn't have
+# support for running arbitrary install scripts
+# TODO: ^^^that might not actually be true
 from setuptools import setup
 
-setup_args = dict(
-    name="poetry-kernel",
-    packages=["poetry_kernel"],
-)
+current_dir = os.path.abspath(os.path.dirname(__file__))
+setup_args = dict()
 
+# Package the kernel.json file
 # Note: this was adapted from the ipython/ipykernel setup.py script
 # https://github.com/ipython/ipykernel/blob/abefee4c935ee79d3821dfda02f1511f55d4c996/setup.py#L95
 # (Modified BSD License)
-if any(a.startswith(('bdist', 'install')) for a in sys.argv):
-    current_dir = os.path.abspath(os.path.dirname(__file__))
+if any(a.startswith(("bdist", "install")) for a in sys.argv):
     sys.path.insert(0, current_dir)
 
     spec_dir = os.path.join(current_dir, "data_kernelspec")
@@ -33,4 +34,38 @@ if any(a.startswith(('bdist', 'install')) for a in sys.argv):
         ),
     ]
 
-setup(**setup_args)
+with open(os.path.join(current_dir, "README.md")) as fp:
+    README = fp.read()
+
+setup(
+    name="poetry-kernel",
+    version="0.1.0",
+
+    # Package metadata
+    author="Pathbird Inc",
+    author_email="oss@pathbird.com",
+    url="https://github.com/pathbird/poetry-kernel",
+    license="MIT",
+    description="Python Jupyter kernel using Poetry for dependency management",
+    long_description=README,
+    keywords=["Interactive", "Interpreter", "Shell", "Web"],
+    classifiers=[
+        "Framework :: Jupyter",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+    ],
+
+    # Actual code stuff
+    packages=["poetry_kernel"],
+    python_requires=">=3.7",
+    install_requires=[
+        "jupyter-client ~= 7.0.1",
+        "colorama ~= 0.4.4",
+    ],
+    **setup_args,
+)
